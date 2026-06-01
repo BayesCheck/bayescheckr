ce175cd09c8dd20ddf88eda1d068d2cdbdbe1a09
Lance Browne <lancebrowne@Lances-MacBook-Pro-3.local>
2026-06-01 14:32:32 -0400

    Updated to an approximate of final functions. Final function soon

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,36 @@
 #' prior
 #'
-#' wrapper for prior
-#' @param prior_input
-prior_sampler <- function(prior_input){
-  prior_input
+#' Draws 1 from the prior. relies on having no other inputs than the function and drawing from it once
+#' @param prior_input the function that details the actual distribution function
+#' @param prior_hyperparams the vector of hyperparameters
+prior_sampler <- function(prior_input, prior_hyperparams){
+  prior_sample <- prior_input(prior_hyperparams)
+  return(prior_sample)
 }
 
-#' likelyhood
+
+#' likelihood
 #'
-#' wrapper for likelyhood
-#' @param likelyhood_input
-likelyhood_sampler <- function(likelyhood_input){
-  likelyhood_input
+#'Draws N times from the likelyhood using the draw from the prior
+#'NOTE: format the output later
+#' @param n_obs number of observations
+#' @param prior_samp the value drawn from the prior
+#' @param likelihood_input function that tells the code what distribution to draw from and how many times
+likelihood_sampler <- function(n_obs, prior_samp, likelihood_input) {
+  likelihood_draws <- as.vector(likelihood_input(n_obs, prior_samp))
+  return(likelihood_draws)
 }
+
 
 #' posterior_input
 #'
-#' wrapper for posterior_input
-#' @param posterior_input_function
-posterior_input_sampler <- function(posterior_input_function){
-  posterior_input_function
+#' The posterior given to us to validate, as opposed to the one we approximate with gibbs.
+#' NOTE: format the output later
+#' @param y the vector of likelyhood draws
+#' @param n_obs number of observations
+#' @param posterior_input_function whatever the user thinks the posterior draws are supposed to be.
+#' @param prior_hyperparams the vector of hyperparameters
+posterior_input_sampler <- function(y, n_obs, posterior_input_function, prior_hyperparams){
+  posterior_matrix <- posterior_input_function(y, n_obs, prior_hyperparams)
+  return(posterior_matrix)
 }

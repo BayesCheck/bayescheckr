592639d299abbcefea42f5d623a1f25c34bfcca4
Lance Browne <lancebrowne@Lances-MacBook-Pro-3.local>
2026-06-01 12:41:45 -0400

    utils start

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' prior
+#'
+#' wrapper for prior
+#' @param prior_input
+prior <- function(prior_input){
+  prior_input
+}
+
+#' likelyhood
+#'
+#' wrapper for likelyhood
+#' @param likelyhood_input
+likelyhood <- function(likelyhood_input){
+  likelyhood_input
+}
+
+#' posterior_input
+#'
+#' wrapper for posterior_input
+#' @param posterior_input_function
+posterior_input <- function(posterior_input_function){
+  posterior_input_function
+}

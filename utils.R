fc7e2f0c55266f53069f2269a303dd25498da77c
Lance Browne <lancebrowne@Lances-MacBook-Pro-3.local>
2026-06-01 12:24:49 -0400

    created wrappers for prior, likelyhood, posterior

---
+++ utils.R
@@ -0,0 +1,24 @@
+#'prior
+#'
+#'storing the prior in a wrapper
+#'@param prior_function
+prior <- function(prior_function){
+  prior_function 
+}
+
+
+#'likelyhood
+#'
+#'storing the likelyhood in a wrapper
+#'@param likelyhood_function
+likelyhood <- function(likelyhood_function){
+  likelyhood_function 
+}
+
+#'input_posterior
+#'
+#'storing the posterior that we are checking in a wrapper
+#'@param input_posterior_function
+input_posterior <- function(input_posterior_function){
+  input_posterior_function
+}

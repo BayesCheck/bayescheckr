1015417c1f18cf328005fc9e80aaf559e6bf2f67
Rafael Mautner <rafael.mautner@icloud.com>
2026-06-04 10:31:50 -0400

    onLoad removed

--- R/sbc.R
+++ R/sbc.R
@@ -48,6 +48,11 @@ run_sbc <- function(prior_sampler,
                     prior_hyper_params,
                     parallelize = FALSE){
 
+  # register S3 method at call time, not load time
+  registerS3method("SBC_fit", "bayescheckr_backend",
+                   SBC_fit.bayescheckr_backend,
+                   envir = asNamespace("SBC"))
+
   #0. Validate stuff:
   theta_test <- prior_sampler(prior_hyper_params)
   if (!is.numeric(theta_test))
@@ -119,13 +124,5 @@ ranks_to_sbc_results <- function(ranked) {
     ),
     class = "SBC_results"
   )
-}
-
-.onLoad <- function(libname, pkgname) {
-  # directly inject our method into the SBC package environment
-  sbc_env <- asNamespace("SBC")
-  assign("SBC_fit.bayescheckr_backend",
-         SBC_fit.bayescheckr_backend,
-         envir = sbc_env)
 }
 
